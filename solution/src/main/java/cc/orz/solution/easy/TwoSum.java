5a541ff1e7b8c279cd1dbfb18d31c66939a55691
hongwei <skhizein0706@gmail.com>
2018-10-29 02:44:25 +0800

    add 两数之和

---
+++ solution/src/main/java/cc/orz/solution/easy/TwoSum.java
@@ -0,0 +1,22 @@
+package cc.orz.solution.easy;
+
+/**
+ * @author hongweixu
+ * @since 2018/10/29 02:33
+ */
+public class TwoSum {
+    /**
+     * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+     * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+     * <p>
+     * 示例:
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     */
+    public int[] twoSum(int[] nums, int target) {
+        // FIXME
+
+        return null;
+    }
+}
